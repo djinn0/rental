e772a26411f50b6a501cf62d8bcfe1f4aa0dc05b
djinn0 <dsr.1991@gmail.com>
2026-02-27 13:06:23 -0700

    Update footer links and styling identifiers

--- footer.js
+++ footer.js
@@ -11,10 +11,11 @@ function generateFooter()
   let d = new Date();
   let n = d.getFullYear(); /* Adds current year to footer */
   let e1 = '<a href="contact.html">Contact</a>';
-  let e2 = '<a href="disclosure.html">Escapade Outfitters &copy' + n + '</a>';
-  let element = '<div id="element">' + e1 + e2 + '</div>';
-  let anchor = '<div id="mountain">' + "\u26F0"; + '</div>'; 
-  x.insertAdjacentHTML('beforeend', '<div class="footer" id="footer">' + element + mountain + '</div>');
+  let e2 = '<a href="disclosure.html">Sex Toy Ahoy &copy' + n + '</a>';
+  let e3 = '<a href="disclosure.html">Disclosure</a>';
+  let element = '<div id="element">' + e1 + e2 + e3 + '</div>';
+  let anchor = '<div id="anchor">' + "\u2693" + '</div>';
+  x.insertAdjacentHTML('beforeend', '<div class="footer" id="footer">' + element + anchor + '</div>');
   
   /* styling */
   var s = document.getElementsByTagName("style")[0];
@@ -30,12 +31,12 @@ function generateFooter()
   } else {
     var sf1 = 'div.footer {position: static; margin-top: 1.5vh; z-index: 11; width: 100%; background-color: #333; }';
   }
-  var sf2 = '#mountain{color: white; font-size: 1.75em; text-align: center;}';
+  var sf2 = '#anchor{color: white; font-size: 1.75em; text-align: center;}';
   var sf3 = '#element { padding: 8px 40px; display: flex; justify-content: space-between; }';
   var sf4 = '#element > a { color: white; text-decoration: none; font-size: 1.5em; display: inline-block; }';
   /* styling mobile */
   var sm1 = 'div.footer{margin-top: 0;}';
-  var sm2 = '#mountain{color: white; font-size: 6vmin; text-align: center;}';
+  var sm2 = '#anchor{color: white; font-size: 6vmin; text-align: center;}';
   var sm3 = '#element > a { color: white; text-decoration: none; font-size: 4vmin; display: inline-block; }';
   s.insertAdjacentHTML('beforeend', sf1 + sf2 + sf3 + sf4 );
   s.insertAdjacentHTML('beforeend', '@media only screen and (max-width: 1000px){' + sm1 + sm2 + sm3 +'}');
